7566c7e93762d57a7fbb3c35588a58e06eafc934
jbx202 <jbx202@nyu.edu>
2026-02-27 04:29:03 -0500

    completed login and register pages

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import pymongo
 from bson.objectid import ObjectId
+import datetime
+from pymongo.errors import DuplicateKeyError
+
 import os
 from dotenv import load_dotenv
-from flask import  render_template, redirect, url_for, request, Flask
-from flask_login import LoginManager, login_user, logout_user, UserMixin, login_required
+
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify, Flask
+from flask_login import login_required, current_user
+
 from db import db, users, habits
+
 from components.dashboard import dashboard_bp
 
 load_dotenv()
@@ -16,33 +23,38 @@ def app():
     #link app to dashboard.py
     app.register_blueprint(dashboard_bp)
 
+    #Mongo connection
+    from db import db, users, habits
 
     app.db = db
     app.users = users
     app.habits = habits
 
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-
-    #load in user id after login
-    @login_manager.user_loader
-    def loadUser(user_id):
-        existing = users.find_one({"_id": ObjectId(user_id)})
-        if not existing:
-            return None
-        user = User()
-        user.id = str(existing("_id"))
-        user.username = existing.get("username")
-        return user
+    #login stuff
+    from flask_login import LoginManager, login_user, logout_user, UserMixin
+    login = LoginManager()
+    login.init_app(app)
 
 
-    #catching errors
-    # @app.errorhandler(Exception)
-    # def handle_exception(e):
-    # return render_template("error.html", error=e)
     #for login stuff
     class User(UserMixin):
         pass
+
+    #load in user id after login
+    @login.user_loader
+    def loadUser(user_id):
+        data = users.find_one({"_id": ObjectId(user_id)})
+        
+        user = User()
+        user.id = str(data["_id"])
+        user.username = data["username"]
+
+        return user
+    
+    #catching errors
+    # @app.errorhandler(Exception)
+    # def handle_exception(e):
+    #     return render_template("error.html", error=e)
 
     @app.route("/")
     def home():
@@ -55,16 +67,45 @@ def app():
 
         username = request.form.get("username")
         password = request.form.get("password")
-
+    
         existingUser = users.find_one({"username": username})
         if existingUser and existingUser["password"] == password:
             user = User()
             user.id = str(existingUser["_id"])
             user.username = existingUser["username"]
+            
             login_user(user)
+            print("I have logged in")
             return redirect(url_for("dashboard.dashboard"))
         else:
-            return render_template("login.html", error="Invalid username or password.")
+            return render_template("login.html", error="User/Password")
+
+    @app.route("/register", methods=["POST", "GET"])
+    def signup():
+        if request.method == "GET":
+            return render_template("register.html")
+
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        existingUser = users.find_one({"username": username})
+        existingEmail= users.find_one({"email": email})
+
+        if existingUser:
+            return render_template("register.html", error="Username already exists.")
+        if existingEmail:
+            return render_template("register.html", error="Email already exists.")
+        
+        newUser = {
+            "username": username,
+            "email": email,
+            "password": password
+        }
+
+        users.insert_one(newUser)
+
+        return render_template("login.html", message="Registration complete! Please log in.")
 
     #logout stuff
     @app.route("/logout")
@@ -76,11 +117,13 @@ def app():
 
 app_instance = app()
 
-#I have no idea what this is but it seems necessary
-#lowk copy pasted professors code from example app
+#I have no idea what this is but it seems necessary 
+#lowk copy pasted professors code from example app 
 if __name__ == "__main__":
-    FLASK_PORT = int(os.getenv("FLASK_PORT", "5000"))
+    FLASK_PORT = os.getenv("FLASK_PORT", "5000")
     FLASK_ENV = os.getenv("FLASK_ENV")
     print(f"FLASK_ENV: {FLASK_ENV}, FLASK_PORT: {FLASK_PORT}")
 
-    app_instance.run(port=FLASK_PORT)
+    app_instance.run(port=FLASK_PORT) 
+
+    
